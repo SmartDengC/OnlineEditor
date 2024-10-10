017526fceaf87a85f1e77f3ef5e3124d6a5c8b65
smartdengc <zero.dengc@outlook.com>
2024-10-10 16:58:24 +0800

    feat: editor第一版

---
+++ src/views/MonacoEditorView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>MonacoEditor</h1>
+    <select v-model="choiceLanguage">
+      <option
+        v-for="option in language"
+        :key="option.value"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+    <monaco-editor :language="choiceLanguage"></monaco-editor>
+  </div>
+</template>
+
+<script>
+import MonacoEditor from "@/components/MonacoEditorComponents";
+export default {
+  name: "Father",
+  data() {
+    return {
+      choiceLanguage: "python", // 下拉框选中的语言， 默认选中的数据
+      language: [
+        { label: "java", value: "java" },
+        { label: "python", value: "python" },
+        { label: "javascript", value: "javascript" },
+        { label: "go", value: "go" },
+      ],
+    };
+  },
+  components: {
+    MonacoEditor,
+  },
+};
+</script>
